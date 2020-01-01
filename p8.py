fe5821f79354c3711caba5375de3397f5fb62d24
rajdhanani <43428556+dhananiraj@users.noreply.github.com>
2020-01-01 09:55:16 +0530

    Update p8.py

--- p8.py
+++ p8.py
@@ -17,3 +17,15 @@ for i in range(0,n):
     for j in range(0,i+1):
         print(j+1,end="")
     print()
+
+#   * 
+#  * * 
+# * * * 
+
+n = int(input("enter n:"))
+for i in range(0,n):
+    for j in range(0 , n - i):
+        print(" ",end="")
+    for j in range(0, i + 1): 
+        print("* ",end="")
+    print()
