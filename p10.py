c72fbc8da858ecddc397bbe699a4729856caaae3
rajdhanani <43428556+dhananiraj@users.noreply.github.com>
2020-01-07 12:40:26 +0530

    Update p10.py

--- p10.py
+++ p10.py
@@ -1,8 +1,11 @@
+def greatest(x,y,z):
+    if x>=y>=z or x>=z>=y:
+        return x
+    elif y>=x>=z or y>=z>=x:
+        return y
+    elif z>=x>=y or z>=y>=x:
+        return z
+
 x,y,z = input("enter 3 number:").split()
 x,y,z = int(x),int(y),int(z)
-if x>=y and y>=z:
-    print("max:",x)
-elif y>=x and x>=z:
-    print("max:",y)
-else:
-    print("max:",z)
+print("max:",greatest(x,y,z))
