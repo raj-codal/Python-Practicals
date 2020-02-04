7f11f3f95fb73c560667ff9258df8f68ace4542a
rajdhanani <43428556+dhananiraj@users.noreply.github.com>
2020-02-04 18:58:58 +0530

    Update p11.py

--- p11.py
+++ p11.py
@@ -1,4 +1,3 @@
-a,b,c = input("Enter a,b,c for ax^2+bx+c=0:").split()
-a,b,c = float(a),float(b),float(c)
+a,b,c = [float(i) for i in input("Enter a,b,c for ax^2+bx+c=0:").split()]
 x1,x2 = (-b + (b ** 2 - 4 * a * c) ** 0.5)/(2*a),(-b - (b ** 2 - 4 * a * c) ** 0.5)/(2*a)
 print(x1,",",x2)
