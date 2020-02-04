6965f3ec0be40118eea6a673a24619178f2761e6
rajdhanani <43428556+dhananiraj@users.noreply.github.com>
2020-02-04 19:25:42 +0530

    Update p14.py

--- p14.py
+++ p14.py
@@ -1,10 +1,10 @@
 def fibonacci(n):
     l = []
-    if n == 1: l = [1]
-    elif n > 1: l = [1,1]
-    if n > 2:
-        for i in range(2,n):
+    if n >= 1: l = [1,1]
+    if n >= 2:
+        while l[-1] <= n:
             l += [(l[-1]+l[-2])]
+        l.pop(-1)
     print(l)
 
 n = int(input("Enter n:"))
